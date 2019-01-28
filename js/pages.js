20aea6fa1c65b99afc50246bd89b347692b350d2
Kwanghoon Kim <raykim8704@gmail.com>
2019-01-29 07:04:24 +0900

    no message

--- js/pages.js
+++ js/pages.js
@@ -6,22 +6,18 @@ $(document).ready(function(){
 
 function setInterface(){
 	
-	$('#pagehead').load('pagetop.html',function(){
-		$('.button-collapse').sideNav();
-	    $('.dropdown-button').dropdown({
-	          inDuration: 300,
-	          outDuration: 225,
-	          constrainWidth: false, // Does not change width of dropdown to that of the activator
-	          hover: true, // Activate on hover
-	          gutter: 0, // Spacing from edge
-	          belowOrigin: true, // Displays dropdown below the button
-	          alignment: 'left', // Displays dropdown with edge aligned to the left of button
-	          stopPropagation: false // Stops event propagation
-	        });
-	    $('.collapsible').collapsible();
-	});
+	$('#pagehead').load('pagetop.html');
 	$('#pagefooter').load('pagefooter.html');
+	$('.kakoinfo').load('kakaoinfo.html');
+	 $('.carousel.carousel-slider').carousel({
+		    fullWidth: true,
+		    indicators: true
+		  });
+//	 $('td').addClass('center-align')
+//	$('th').addClass('center-align')
+	 $('table').addClass('centered');
+	 
 	
-	
+	   
 	
 }
